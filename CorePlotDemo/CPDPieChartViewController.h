c6f6566e1bbae73bc1a0633fb0469f4bda41af8d
Azam Malik <azam@sdsol.com>
2012-12-07 21:53:06 -0500

    second

--- CorePlotDemo/CPDPieChartViewController.h
+++ CorePlotDemo/CPDPieChartViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CPDPieChartViewController : UIViewController
+@interface CPDPieChartViewController : UIViewController <CPTPlotDataSource, UIActionSheetDelegate>
 
 @end
--- CorePlotDemo/CPDPieChartViewController.m
+++ CorePlotDemo/CPDPieChartViewController.m
@@ -1,18 +1,32 @@
-//
-//  CPDFirstViewController.m
-//  CorePlotDemo
-//
-//  Created by Azam Malik on 12/7/12.
-//  Copyright (c) 2012 prepcloud. All rights reserved.
-//
-
 #import "CPDPieChartViewController.h"
 
 @interface CPDPieChartViewController ()
 
+@property (nonatomic, strong) IBOutlet UIToolbar *toolbar;
+@property (nonatomic, strong) IBOutlet UIBarButtonItem *themeButton;
+@property (nonatomic, strong) CPTGraphHostingView *hostView;
+@property (nonatomic, strong) CPTTheme *selectedTheme;
+
+-(IBAction)themeTapped:(id)sender;
+-(void)initPlot;
+-(void)configureHost;
+-(void)configureGraph;
+-(void)configureChart;
+-(void)configureLegend;
 @end
 
 @implementation CPDPieChartViewController
+@synthesize toolbar, themeButton;
+@synthesize hostView = hostView_;
+@synthesize selectedTheme = selectedTheme_;
+
+
+#pragma mark - UIViewController lifecycle methods
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    // The plot is initialized here, since the view bounds have not transformed for landscape until now
+    [self initPlot];
+}
 
 - (void)viewDidLoad
 {
@@ -26,9 +40,109 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - IBActions
+-(IBAction)themeTapped:(id)sender {
+}
+
+#pragma mark - Chart behavior
+-(void)initPlot {
+    [self configureHost];
+    [self configureGraph];
+    [self configureChart];
+    [self configureLegend];
+}
+
+-(void)configureHost {
+    
+    // 1 - Set up view frame
+    CGRect parentRect = self.view.bounds;
+    CGSize toolbarSize = self.toolbar.bounds.size;
+    parentRect = CGRectMake(parentRect.origin.x,
+                            (parentRect.origin.y + toolbarSize.height),
+                            parentRect.size.width,
+                            (parentRect.size.height - toolbarSize.height));
+    // 2 - Create host view
+    self.hostView = [(CPTGraphHostingView *) [CPTGraphHostingView alloc] initWithFrame:parentRect];
+    self.hostView.allowPinchScaling = NO;
+    [self.view addSubview:self.hostView];
+    
+}
+
+-(void)configureGraph {
+    
+    // 1 - Create and initialize graph
+    CPTGraph *graph = [[CPTXYGraph alloc] initWithFrame:self.hostView.bounds];
+    self.hostView.hostedGraph = graph;
+    graph.paddingLeft = 0.0f;
+    graph.paddingTop = 0.0f;
+    graph.paddingRight = 0.0f;
+    graph.paddingBottom = 0.0f;
+    graph.axisSet = nil;
+    // 2 - Set up text style
+    CPTMutableTextStyle *textStyle = [CPTMutableTextStyle textStyle];
+    textStyle.color = [CPTColor grayColor];
+    textStyle.fontName = @"Helvetica-Bold";
+    textStyle.fontSize = 16.0f;
+    // 3 - Configure title
+    NSString *title = @"Portfolio Prices: May 1, 2012";
+    graph.title = title;
+    graph.titleTextStyle = textStyle;
+    graph.titlePlotAreaFrameAnchor = CPTRectAnchorTop;
+    graph.titleDisplacement = CGPointMake(0.0f, -12.0f);
+    // 4 - Set theme
+    self.selectedTheme = [CPTTheme themeNamed:kCPTPlainWhiteTheme];
+    [graph applyTheme:self.selectedTheme];
+}
+
+-(void)configureChart {
+    
+    // 1 - Get reference to graph
+    CPTGraph *graph = self.hostView.hostedGraph;
+    // 2 - Create chart
+    CPTPieChart *pieChart = [[CPTPieChart alloc] init];
+    pieChart.dataSource = self;
+    pieChart.delegate = self;
+    pieChart.pieRadius = (self.hostView.bounds.size.height * 0.7) / 2;
+    pieChart.identifier = graph.title;
+    pieChart.startAngle = M_PI_4;
+    pieChart.sliceDirection = CPTPieDirectionClockwise;
+    // 3 - Create gradient
+    CPTGradient *overlayGradient = [[CPTGradient alloc] init];
+    overlayGradient.gradientType = CPTGradientTypeRadial;
+    overlayGradient = [overlayGradient addColorStop:[[CPTColor blackColor] colorWithAlphaComponent:0.0] atPosition:0.9];
+    overlayGradient = [overlayGradient addColorStop:[[CPTColor blackColor] colorWithAlphaComponent:0.4] atPosition:1.0];
+    pieChart.overlayFill = [CPTFill fillWithGradient:overlayGradient];
+    // 4 - Add chart to graph    
+    [graph addPlot:pieChart];
+}
+
+-(void)configureLegend {
+}
+
 #pragma mark - Rotation
 -(BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
     return (interfaceOrientation == UIInterfaceOrientationLandscapeLeft);
+}
+
+#pragma mark - CPTPlotDataSource methods
+-(NSUInteger)numberOfRecordsForPlot:(CPTPlot *)plot {
+    return 0;
+}
+
+-(NSNumber *)numberForPlot:(CPTPlot *)plot field:(NSUInteger)fieldEnum recordIndex:(NSUInteger)index {
+    return 0;
+}
+
+-(CPTLayer *)dataLabelForPlot:(CPTPlot *)plot recordIndex:(NSUInteger)index {
+    return nil;
+}
+
+-(NSString *)legendTitleForPieChart:(CPTPieChart *)pieChart recordIndex:(NSUInteger)index {
+    return @"";
+}
+
+#pragma mark - UIActionSheetDelegate methods
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
 }
 
 @end
